c54c830e54ed8319d2f8c06dd4a93ea27a7ea3dd
Lucas <lucas.raziel@gmail.com>
2020-04-10 15:49:12 -0300

    Class 06 - Properties

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header/>
-      <Header/>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Project</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+        </ul>
+      </Header>
     </>
   )
 }
